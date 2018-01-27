9740bd91452d4ab63d08aaa17ba8e46aa9f7bd60
greyfertich <greyfertich@gmail.com>
2018-01-27 13:48:15 -0500

    Update GUI_setup.py

--- GUI_setup.py
+++ GUI_setup.py
@@ -7,10 +7,13 @@ from Paint import *
 import scipy.misc
 from Train import *
 import threading
+from queue import Queue
 
+plt.style.use('ggplot')
 epoch = -1
 root = Tk()
 root.geometry("750x600")
+q = Queue()
 
 def rightArrow(event):
     slide.set((slide.get() + 1))
@@ -20,21 +23,24 @@ def leftArrow(event):
 
 def askQuestion():
     question.pack_forget()
-    lab.pack(fill=X, side=TOP)
+    direction.pack(fill=X)
+    lab.pack(fill=X, side=BOTTOM)
     slide.pack(side=BOTTOM, fill=X)
-    root.bind('<Return>', getValue)
+    root.bind('<Return>', lambda event: getValue(q))
     root.bind('<Right>', rightArrow)
     root.bind('<Left>', leftArrow)
 
 frame = Frame(root, width=1000, height=500, bd=0, bg='#2A353B')
-title = Label(frame, text='Letter Reader', font=("Times", 64), fg='green', bg='#2A353B', bd=0)
+title = Label(frame, text='Print Guesser', font=("Times", 64), fg='green', bg='#2A353B', bd=0)
 question = Button(frame, text='Train', font=("Times", 16), highlightthickness=0, fg='green', bg='#394549', bd=0, activeforeground='#394549', activebackground='#2A353B', command=askQuestion)
-lab = Label(text="Press Enter To Make Selection", font=('Times', 14), bg='#2A353B', bd=0, fg='dark gray')
-slide = Scale(frame, orient=HORIZONTAL, font=("Times", 12), from_=1, to=50, fg='green', bd=0, bg='#2A353B', activebackground='#252F33', troughcolor='#394549', length=400, highlightthickness=0)
-slide.set(26)
+direction = Label(frame, text='Select the number of epochs', font=('Times', 14), bg='#2A353B', bd=0, fg='dark gray')
+lab = Label(frame, text="Press Enter To Make Selection", font=('Times', 14), bg='#2A353B', bd=0, fg='dark gray')
+slide = Scale(frame, orient=HORIZONTAL, font=("Times", 12), from_=1, to=1000, fg='green', bd=0, bg='#2A353B', activebackground='#252F33', troughcolor='#394549', length=400, highlightthickness=0)
+slide.set(25)
+slide_time = Label(frame, text='Approximately 0 seconds/Training duration').pack()
 frame.pack()
 root.configure(background="#2A353B")
-root.title("Letter Reader")
+root.title("Print Guesser")
 title.pack(side=TOP, fill=X)
 question.pack(side=BOTTOM)
 ep, pe, t, eff, est = StringVar(), StringVar(), StringVar(), StringVar(), StringVar()
@@ -43,19 +49,24 @@ title2 = Label(frame, font=('Times', 64), bg='#2A353B', bd=0, text='Training', f
 esttime = Label(frame, font=('Times', 16), bg='#2A353B', fg='gray', bd=0, textvariable=est)
 check = True;
 net = neuralNetwork(1600, 256, 26, 0.01)
+performance_datalist = []
 
 def training_thread(ep, pe, t, eff, est, epochs):
-    global check
+    global check, performance_datalist
     tr = Train(net)
     tr.train(ep, pe, t, eff, est, epochs)
+    performance_datalist = tr.performance_data[:] #Copy list by value
+    performance_datalist.insert(0,0.0)
     check = False
 
-def getValue(self):
+def getValue(q):
+    global epoch
     epoch = slide.get()
     title.pack_forget()
     question.pack_forget()
     slide.pack_forget()
     lab.pack_forget()
+    direction.pack_forget()
     title2.pack(side=TOP, fill=X)
     ep.set('Epoch = 1')
     epoch_counter = Label(left_frame, font=('Times', 24), bg='#2A353B', fg ='green', bd=0, textvariable=ep)
@@ -69,24 +80,25 @@ def getValue(self):
     eff.set('Efficiency = 0')
     effic = Label(left_frame, font=('Times', 24), bg='#2A353B', fg='green', bd=0, textvariable=eff)
     effic.pack(side=BOTTOM)
-    est.set('Time Remaining')
+    est.set('')
     esttime.pack(side=BOTTOM)
-    # f = Figure(figsize=(3,2), dpi=100, facecolor='#2A353B', edgecolor='#00ff00')
-    # a = f.add_subplot(111)
-    # a.plot(range(8), [5,6,3,7,3,6,2,7])
-    # graph = FigureCanvasTkAgg(f, frame)
-    # graph.show()
-    # graph.get_tk_widget().pack()
     left_frame.pack(side=LEFT, fill=X)
     train_thread = threading.Thread(target=training_thread, args=(ep, pe, t, eff, est, epoch))
     train_thread.start()
 
 def continue_after():
+    global epoch, performance_datalist
     if not check:
         frame.pack_forget()
         left_frame.pack_forget()
         esttime.pack_forget()
         title2.pack_forget()
+        plt.plot(range(epoch+1), performance_datalist)
+        plt.xlabel('Number of Epochs')
+        plt.ylabel('Performance (% Correct)')
+        plt.savefig('graph_image.png')
+        im = Image.open('graph_image.png')
+        im.save('graph_image.gif')
         cv = Paint(root, net)
     else:
         root.after(1000, continue_after)
